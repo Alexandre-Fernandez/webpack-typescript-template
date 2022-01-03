9c32b65fcdfb08ca0bbe9201c980d3c4dfc2bd6f
Alexandre Fernandez <79476242+Alexandre-Fernandez@users.noreply.github.com>
2022-01-03 18:43:15 +0100

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -33,25 +33,3 @@ module.exports = {
 		]
 	},
 }
-
-/*
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /.s?css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [
-      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-      // `...`,
-      new CssMinimizerPlugin(),
-    ],
-  },
-  plugins: [new MiniCssExtractPlugin()],
-};
-
-*/
